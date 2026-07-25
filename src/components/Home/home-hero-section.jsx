b32a4a5e13d303ae6e7661ad362a7e9af3b9ff97
Malaika Zafar <malaikazafarwork@gmail.com>
2026-07-25 18:36:06 +0400

    final website

--- src/components/Home/home-hero-section.jsx
+++ src/components/Home/home-hero-section.jsx
@@ -1,28 +1,47 @@
 import "./home-hero-section.css";
-import { Link } from "react-router-dom";
 
-const HomeHeroSection = () => {
+const HeroBanner = () => {
   return (
-    <section className="hero">
-      <div className="hero-overlay"></div>
-
+    <section className="hero-banner">
       <div className="hero-content">
-        <h1 className="main-title hero-title">
-          Your People, Paid Right
+        <h1>
+          YOUR PEOPLE,
+          <br />
+          PAID RIGHT
         </h1>
 
-        <p className="hero-description body-3-2">
-          From managed payroll services to powerful software
-          solutions, we ensure your employees are paid
-          accurately and on time, every time.
+        <div className="hero-divider"></div>
+
+        <p>
+          PROFESSIONAL PAYROLL
+          <br />
+          SERVICES & SOFTWARE
         </p>
 
-        <Link to="/contact" className="hero-btn">
-          Get in touch
-        </Link>
+        <button className="hero-btn">
+          Get a Quote
+        </button>
+      </div>
+
+      <div className="hero-image-section">
+        <svg
+          className="red-curve"
+          viewBox="0 0 1000 700"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="
+              M0,0
+              C40,480 320,700 1000,700
+              L1000,0Z"
+            fill="#e31b23"
+          />
+        </svg>
+
+        <img src = "/assets/images/homeheroimg.png" className = "hero-image" />
       </div>
     </section>
   );
 };
 
-export default HomeHeroSection;
+export default HeroBanner;
